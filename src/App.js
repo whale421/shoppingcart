937334a7dbda64cc6870dada5fd36791a88642f5
whale421 <62377374+whale421@users.noreply.github.com>
2020-08-15 22:38:30 +0800

    shoppingcart

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React,{Fragment,Component }from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const numbers = [
+  "❥❤",
+  "❥❤",
+  "❥❤",
+  "❥❤",
+  "❥❤",
+  "❥❤",
+  "❥❤",
+  "❥❤",
+  ];  
+class shoppingcart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  NumberList(number) {
+    const numbers = number;
+    const listItems = (numbers || []).map((number) =>
+      <div class="inner" key={number.toString()}>
+        <div class="word">
+            {number} 
+        </div>
+        <div class="keypress1">
+            <button>+</button>数量<button>-</button>
+        </div>
+        <div class="keypress2">
+            <button>删除</button>
+        </div>
+      </div>
+    );
+    return (
+      <ul>{listItems}</ul>
+    );
+  }
+  render() { 
+    return ( 
+      <div class="periphery">
+        {this.NumberList(numbers)}
+      </div>
+     );
+  }
 }
+ 
+export default shoppingcart;
 
-export default App;
